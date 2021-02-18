233fe56fbab713fc2354e9105e37ab034b7c156d
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-02-18 10:56:12 -0600

    got apikey

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -27,12 +27,7 @@ struct ContentView: View {
     }
     
     func getJokes() {
-            jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                              punchline: "Because DEC 25 = OCT 31"))
-            jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                              punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-            jokes.append(Joke(setup: "There are 10 types of people in the world",
-                              punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=8b767f7e74mshdfc34e279a69669p179c3fjsna468a7691543"
         }
 }
 
